f6857ccb8a0860052910fb7e9111e7e6e7202eed
Stojan <mmrsucks@hotmail.com>
2022-06-13 19:33:44 +0200

    Initial commit

---
+++ src/dd/Domasna.java
@@ -0,0 +1,44 @@
+package dd;
+
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
+public class Domasna {
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static class Domasno {
+
+        public Domasno() {
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello");
+            ;// comment
+
+            System.out.println("How are you??");
+
+            ;// comment
+
+            System.out.println("GoodBye");
+
+        }
+
+    }
+}
+
+
